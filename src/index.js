8fa73e008f933ecc671dd08c96e2ed2a4933303f
Klyment <klim86kr@gmail.com>
2023-04-18 16:33:36 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -10,10 +10,11 @@ const input = document.getElementById('search-box');
 const ul = document.querySelector('country-list');
 
 const fetchCountries = debounce(async (searchQuery, callback) => {
-    if (!searchQuery.trim())
-        return; // якщо поле пошуку пусте, повернути порожній масив
+  if (!searchQuery.trim()) 
+  return; // якщо поле пошуку пусте, повернути порожній масив
   try {
-    const response = await axios.get(`https://restcountries.com/v2/name/${searchQuery}`, {
+    // const response = await axios.get(`https://restcountries.com/v2/name/${searchQuery}`, {
+    const response = await axios.get(`https://restcountries.com/v3.1/name/${searchQuery}`, {
       params: {
         fields: 'name,flags.svg,capital,population,languages.name' // вибір полів, що повертаються
       }
