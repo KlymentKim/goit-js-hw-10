4bc77803571f4a51cabf4035baf1076add73029c
Klyment <klim86kr@gmail.com>
2023-04-18 16:11:22 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import './css/styles.css';
+import axios from 'axios';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
-const urlLink = 'https://restcountries.com/v3.1/name/{name}';
+// const urlLink = 'https://restcountries.com/v3.1/name/{name}';
+
+const input = document.getElementById('search-box');
+const ul = document.querySelector('country-list');
+
+const fetchCountries = debounce(async (searchQuery, callback) => {
+    if (!searchQuery.trim())
+        return; // якщо поле пошуку пусте, повернути порожній масив
+  try {
+    const response = await axios.get(`https://restcountries.com/v2/name/${searchQuery}`, {
+      params: {
+        fields: 'name,flags.svg,capital,population,languages.name' // вибір полів, що повертаються
+      }
+    });
+    callback(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+}, 300);
+
+export default fetchCountries;
