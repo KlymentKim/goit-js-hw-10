a584359b02fb7e528d09614ddab96c0725551d63
Klyment <klim86kr@gmail.com>
2023-04-19 17:08:11 +0300

    fix

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -17,3 +17,11 @@ const fetchCountries = debounce(async (searchQuery, callback) => {
 }, 300);
 
 export default fetchCountries;
+
+// async function fetchCountries(name) {
+//   const response = await fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags.svg,languages`);
+//   const data = await response.json();
+//   return data;
+// }
+
+// export { fetchCountries };
