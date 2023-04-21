f82868acc73f755007e067954eb2e05520051f7c
Klyment <klim86kr@gmail.com>
2023-04-21 17:07:38 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
 
-// const DEBOUNCE_DELAY = 300;
 
-
-const searchBox = document.getElementById('search-box');
+const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
