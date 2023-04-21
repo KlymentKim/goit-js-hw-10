9789e5d01efca941c0a861de24a777ab69830848
Klyment <klim86kr@gmail.com>
2023-04-21 12:17:32 +0300

    fix

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -18,36 +18,3 @@
 
 // export default fetchCountries;
 
-// fetchCountries.js
-// export const fetchCountries = async (name) => {
-//   try {
-//     const response = await fetch(`https://restcountries.com/v2/name/${name}?fields=name;capital;population;flags.svg;languages`);
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     const countries = await response.json();
-//     return countries;
-//   } catch (error) {
-//     throw new Error(`HTTP error: ${error}`);
-//   }
-// };
-
-
-// async function fetchCountries(name) {
-//   const response = await fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags.svg,languages`);
-//   const data = await response.json();
-//   return data;
-// }
-
-// export { fetchCountries };
-
-// export const fetchCountries = name => {
-//   return fetch(
-//     `https://restcountries.com/v2/name/${name}?fields=name;capital;population;flags.svg;languages`
-//   ).then(response => {
-//     if (response.ok) {
-//       return response.json();
-//     }
-//     throw new Error(response.statusText);
-//   });
-// };
